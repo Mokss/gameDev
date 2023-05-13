4197ca73a1bc6c2bf1130b580c3e57149f09df06
Mokss <moksshuv@yandex.ru>
2023-05-13 19:32:13 +0400

    feat: add hitbox

--- platformer/src/classes/player.ts
+++ platformer/src/classes/player.ts
@@ -1,5 +1,5 @@
 import { gravity } from '../constants.js';
-import type { Position } from '../types';
+import type { Block, Position } from '../types';
 import { CollisionBlock } from './collisionBlock.js';
 import { isCollision } from '../utils.js';
 import { SpriteProps, Sprite } from './sprite.js';
@@ -12,6 +12,7 @@ export interface PlayerProps extends SpriteProps {
 export class Player extends Sprite {
 	velocity: Position;
 	collisionBlocks: CollisionBlock[] = [];
+	hitbox: Block;
 
 	constructor(props: PlayerProps) {
 		super({ ...props, scale: 0.5 });
@@ -20,17 +21,35 @@ export class Player extends Sprite {
 			y: 1,
 		};
 		this.collisionBlocks = props.collisionBlocks;
+		this.hitbox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: 10,
+			height: 10
+		};
 	}
 
 	update() {
 		this.updateFrames();
-		this.context.fillStyle = 'rgba(0, 255, 0, 0.3)';
+		// this.updateHitbox();
+		
+		// NOTE: img borders
+		this.context.fillStyle = 'rgba(0, 255, 0, 0.2)';
 		this.context.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+		// NOTE: hitbox borders
+		this.context.fillStyle = 'rgba(255, 0, 0, 0.2)';
+		this.context.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height);
+
 		this.draw();
 
 		this.position.x += this.velocity.x;
+		this.updateHitbox();
 		this.checkHorizontalCollision();
 		this.applyGravity();
+		this.updateHitbox();
 		this.checkVerticalCollision();
 	}
 
@@ -44,16 +63,22 @@ export class Player extends Sprite {
 		for (let i = 0; i < this.collisionBlocks.length; i++) {
 			const collisionBLock = this.collisionBlocks[i];
 
-			if (isCollision(this, collisionBLock)) {
+			if (isCollision(this.hitbox, collisionBLock)) {
 				if (this.velocity.x > 0) { 
 					this.velocity.x = 0;
-					this.position.x = collisionBLock.position.x - this.width - 0.01;
+
+					const offset = this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+					this.position.x = collisionBLock.position.x - offset - 0.01;
 					break;
 				}
 
 				if (this.velocity.x < 0) { 
 					this.velocity.x = 0;
-					this.position.x = collisionBLock.position.x + collisionBLock.width + 0.01;
+
+					const offset = this.hitbox.position.x - this.position.x;
+
+					this.position.x = collisionBLock.position.x + collisionBLock.width - offset + 0.01;
 					break;
 				}
 			}
@@ -65,19 +90,36 @@ export class Player extends Sprite {
 			const collisionBLock = this.collisionBlocks[i];
 
 
-			if (isCollision(this, collisionBLock)) {
+			if (isCollision(this.hitbox, collisionBLock)) {
 				if (this.velocity.y > 0) { 
 					this.velocity.y = 0;
-					this.position.y = collisionBLock.position.y - this.height - 0.01;
+
+					const offset = this.hitbox.position.y - this.position.y + this.hitbox.height;
+
+					this.position.y = collisionBLock.position.y - offset - 0.01;
 					break;
 				}
 
 				if (this.velocity.y < 0) { 
 					this.velocity.y = 0;
-					this.position.y = collisionBLock.position.y + collisionBLock.height + 0.01;
+
+					const offset = this.hitbox.position.y - this.position.y;
+
+					this.position.y = collisionBLock.position.y + collisionBLock.height - offset + 0.01;
 					break;
 				}
 			}
 		}
+	}
+
+	updateHitbox() {
+		this.hitbox = {
+			position: {
+				x: this.position.x + 34,
+				y: this.position.y + 26,
+			},
+			width: 15,
+			height: 27
+		};
 	}
 }
