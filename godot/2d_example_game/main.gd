92e8cfb0f1ca77b115c2639564ba77102c4d226a
Maksim Shuvaev <moksshuv@yandex.ru>
2024-04-20 16:36:36 +0300

    наконец то закончил первый туториал с 2д

--- godot/2d_example_game/main.gd
+++ godot/2d_example_game/main.gd
@@ -6,14 +6,19 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-func _ready():
-	new_game()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_player_hit():
 	game_over()
@@ -38,6 +43,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
