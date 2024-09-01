9256453b30c58c4d4e350307fb77428073101233
Maksim Shuvaev <moksshuv@yandex.ru>
2024-09-01 18:22:19 +0300

    ну почему в плюсах так сложно либы ставить?

---
+++ learnOpenGL/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	std::cout << "наконец то я настроил зависимости, сцуууука";
+	return 0;
+}
