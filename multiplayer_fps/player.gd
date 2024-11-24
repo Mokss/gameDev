20bec07b3b7f92093efad2cff356ca9de3508bff
Maksim Shuvaev <moksshuv@yandex.ru>
2024-11-24 19:13:46 +0300

    finish first online fps

--- multiplayer_fps/player.gd
+++ multiplayer_fps/player.gd
@@ -1,27 +1,48 @@
 extends CharacterBody3D
 
+signal  health_changed(health_value)
+
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
 @onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
+@onready var rayCast = $Camera3D/RayCast3D
+
+var health = 3
 
 const SPEED = 10.0
 const JUMP_VELOCITY = 10.0
 const GRAVITY = 20.0
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(str(name).to_int())
+
 func _ready() -> void:
+	if not is_multiplayer_authority():
+		return
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
+	
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * 0.005)
 		camera.rotate_x(-event.relative.y * 0.005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 	if Input.is_action_just_pressed("shoot") \
 			and anim_player.current_animation != 'shoot':
-		play_shoot_effects()
+		play_shoot_effects.rpc()
+		if rayCast.is_colliding():
+			var hit_player = rayCast.get_collider()
+			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 		
 
 func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
@@ -50,8 +71,23 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+@rpc("call_local")
 func play_shoot_effects():
 	anim_player.stop()
 	anim_player.play("shoot")
 	muzzle_flash.restart()
 	muzzle_flash.emitting = true
+
+@rpc("any_peer")
+func receive_damage(): 
+	health -= 1
+	
+	if health <= 0:
+		health = 3
+		position = Vector3.ZERO
+
+	health_changed.emit(health)
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == 'shoot': 
+		anim_player.play('idle')
