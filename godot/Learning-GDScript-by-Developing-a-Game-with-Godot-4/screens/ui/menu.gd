de055cd536615aa42abdf884ba92c46229a2ad4f
Maksim Shuvaev <moksshuv@yandex.ru>
2025-01-07 00:40:18 +0300

    7.01.2025

---
+++ godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/screens/ui/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://screens/game/main.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
