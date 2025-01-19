66043c72c8582f0be6acd5c0b428bf0c99c8f21e
Maksim Shuvaev <moksshuv@yandex.ru>
2025-01-19 23:42:37 +0300

    19.01.2025

--- godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/screens/game/main.gd
+++ godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/screens/game/main.gd
@@ -3,9 +3,20 @@ extends Node2D
 @onready var _game_over_menu: CenterContainer = $CanvasLayer/GameOverMenu
 @onready var _enemy_spawner: EntitySpawner = $EnemySpawner
 @onready var _health_potion_spawner: EntitySpawner = $HealtSpawner
+@onready var _time_label: Label = $ CanvasLayer/TimerUI/TimeLabel
+var _time: float = 0.0:
+	set(value):
+		_time = value
+		_time_label.text = str(floor(_time))
 
 func _on_player_died() -> void:
-	print("умер блять")
 	_enemy_spawner.stop_timer()
 	_game_over_menu.show()
 	_health_potion_spawner.stop_timer()
+	
+	set_process(false)
+	@warning_ignore("narrowing_conversion")
+	HighscoreManager.set_new_highscore(_time)
+
+func _process(delta: float):
+	_time += delta
--- godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/screens/ui/menu.gd
+++ godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/screens/ui/menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var highscore_label: Label = $CenterContainer/MainUiContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/HighscoreLabel
+
+func _ready():
+	highscore_label.text = "Highscore: " + str(HighscoreManager.highscore)
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://screens/game/main.tscn")
