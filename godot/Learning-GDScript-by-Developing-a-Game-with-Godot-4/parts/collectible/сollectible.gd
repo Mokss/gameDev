938869ba0fc0a1ffbfe4803f41cd5cb4414b171e
Maksim Shuvaev <moksshuv@yandex.ru>
2025-01-20 22:19:53 +0300

    20.01.2025

--- godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/parts/collectible/сollectible.gd
+++ godot/Learning-GDScript-by-Developing-a-Game-with-Godot-4/parts/collectible/сollectible.gd
@@ -1,6 +1,4 @@
-extends Node2D
+class_name Collectible extends Node2D
 
-class_name Collectible
-
-func _on_area_2d_body_entered(body: Player) -> void:
+func _on_area_2d_body_entered(_body: Player) -> void:
 	pass # Replace with function body.
