1b52599c212391e114386d2df096a6fb06df574c
Maksim Shuvaev <moksshuv@yandex.ru>
2025-01-24 01:06:52 +0300

    24.01.2025

--- godot/gamedev_tv_2d_speedy_saucer/player.gd
+++ godot/gamedev_tv_2d_speedy_saucer/player.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
 
+@export var speed = 400
+var start_borders
+var end_borders
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_borders = Vector2(27, 34)
+	end_borders = get_viewport_rect().size - start_borders
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_bottom"):
-		velocity.y = 100;
-	if Input.is_action_pressed("move_top"):
-		velocity.y = -100;
-	if Input.is_action_just_pressed("move_left"):
-		velocity.x = -100;
-	if Input.is_action_just_pressed("move_right"):
-		velocity.x = 100;
+	velocity = Vector2.ZERO;
 	
-	print('Velsicy', velocity)
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	velocity = velocity.normalized() * speed
 	
-	velocity.normalized()
-	
-	print('VelosityNormilize', velocity)
-	
+	# оптикаем предметы с которыми коллизия
 	move_and_slide()
+	# не даем выйти за рамки экрана
+	position = position.clamp(start_borders, end_borders)
+	
