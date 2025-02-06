1661cc5968d6ccc1d6c51bcf3e5ae5f6b3d80a81
Maksim Shuvaev <moksshuv@yandex.ru>
2025-02-06 18:15:05 +0300

    06.02.2025

--- godot/martin-mike/script/player.gd
+++ godot/martin-mike/script/player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump"):
 		jump(JUMP_VELOCITY)
 
 	var direction := Input.get_axis("move_letft", "move_right")
