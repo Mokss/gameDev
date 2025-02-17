9286689451b698d4ccccc51b227e47eaaf0e11c7
Maksim Shuvaev <moksshuv@yandex.ru>
2025-02-17 20:24:53 +0300

    17.02.2025

---
+++ project-boost/Level/landing_pad.gd
@@ -0,0 +1,3 @@
+class_name LandingPad extends CSGBox3D
+
+@export_file("*.tscn") var file_path: String
--- project-boost/player.gd
+++ project-boost/player.gd
@@ -1,9 +1,33 @@
 extends RigidBody3D
 
+@export var thrust: float = 1000.0
+@export var torque_thrust: float = 100.0
+
 func _process(delta: float) -> void:
 	if (Input.is_action_pressed("boost")):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 	if (Input.is_action_pressed("rotate_left")):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 	if (Input.is_action_pressed("rotate_right")):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+	
+	if (Input.is_action_just_pressed("reload")):
+		get_tree().reload_current_scene()
+
+
+func _on_body_entered(body: Node) -> void:
+	var groups: Array[StringName] = body.get_groups()
+	print(groups)
+	
+	if body is LandingPad:
+		print("you win")
+		success(body.file_path)
+	
+	if "Hazard" in groups:
+		crash()
+
+func crash() -> void:
+	get_tree().reload_current_scene()
+
+func success(next_file_path: String) -> void:
+	get_tree().change_scene_to_file(next_file_path)
