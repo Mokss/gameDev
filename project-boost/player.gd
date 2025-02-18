0b0e7d2210dcf16562499f7b04e4d1c9fb17a7c9
Maksim Shuvaev <moksshuv@yandex.ru>
2025-02-18 16:30:58 +0300

    16.02.2025

--- project-boost/player.gd
+++ project-boost/player.gd
@@ -3,6 +3,8 @@ extends RigidBody3D
 @export var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 func _process(delta: float) -> void:
 	if (Input.is_action_pressed("boost")):
 		apply_central_force(basis.y * delta * thrust)
@@ -16,18 +18,27 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	var groups: Array[StringName] = body.get_groups()
-	print(groups)
-	
-	if body is LandingPad:
-		print("you win")
-		success(body.file_path)
-	
-	if "Hazard" in groups:
-		crash()
+	if is_transitioning == false:
+		var groups: Array[StringName] = body.get_groups()
+		print(groups)
+		
+		if body is LandingPad:
+			print("you win")
+			success(body.file_path)
+		
+		if "Hazard" in groups:
+			crash()
 
 func crash() -> void:
-	get_tree().reload_current_scene()
+	is_transitioning = true
+	set_process(false)
+	var tween: Tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
 func success(next_file_path: String) -> void:
-	get_tree().change_scene_to_file(next_file_path)
+	is_transitioning = true
+	set_process(false)
+	var tween: Tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_file_path))
