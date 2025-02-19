9827be4830234ef2d618d72f32d6290e076b4981
Maksim Shuvaev <moksshuv@yandex.ru>
2025-02-19 17:25:10 +0300

    19.02.2025

--- project-boost/player.gd
+++ project-boost/player.gd
@@ -3,15 +3,38 @@ extends RigidBody3D
 @export var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+@onready var explosing_audio: AudioStreamPlayer = $ExplosingAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+
 var is_transitioning: bool = false
 
 func _process(delta: float) -> void:
 	if (Input.is_action_pressed("boost")):
+		booster_particles.emitting = true
+		if rocket_audio.playing == false:
+			rocket_audio.play()
 		apply_central_force(basis.y * delta * thrust)
+	else:
+		booster_particles.emitting = false
+		rocket_audio.stop()
+
 	if (Input.is_action_pressed("rotate_left")):
+		right_booster_particles.emitting = true
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+	else:
+		right_booster_particles.emitting = false
+		
 	if (Input.is_action_pressed("rotate_right")):
+		left_booster_particles.emitting = true
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+	else:
+		left_booster_particles.emitting = false
 	
 	if (Input.is_action_just_pressed("reload")):
 		get_tree().reload_current_scene()
@@ -30,15 +53,19 @@ func _on_body_entered(body: Node) -> void:
 			crash()
 
 func crash() -> void:
+	explosion_particles.emitting = true
+	explosing_audio.play()
 	is_transitioning = true
 	set_process(false)
 	var tween: Tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 
 func success(next_file_path: String) -> void:
+	success_particles.emitting = true
+	success_audio.play()
 	is_transitioning = true
 	set_process(false)
 	var tween: Tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.0)
 	tween.tween_callback(get_tree().change_scene_to_file.bind(next_file_path))
