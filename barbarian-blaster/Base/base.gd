5549685669670057dd798820fafe52dda5ff574a
Maksim Shuvaev <moksshuv@yandex.ru>
2025-03-17 22:55:36 +0300

    17.03.2025

--- barbarian-blaster/Base/base.gd
+++ barbarian-blaster/Base/base.gd
@@ -3,10 +3,21 @@ class_name Base extends Node3D
 @export var max_health: int = 5
 @onready var label_3d: Label3D = $Label3D
 
+var current_healt: int:
+	set(health_in):
+		current_healt = health_in
+		label_3d.text = str(current_healt) + "/" + str(max_health)
+
+		var red = Color.RED
+		var white = Color.WHITE
+		var blended_color = red.lerp(white, float(current_healt) / float(max_health))
+		label_3d.modulate = blended_color
+		
+		if current_healt < 1:
+			get_tree().reload_current_scene()
+
 func _ready() -> void:
-	label_3d.text = str(max_health)
+	current_healt = max_health
 
 func take_damage() -> void:
-	max_health -= 1;
-	label_3d.text = str(max_health)
-	print("damage register")
+	current_healt -= 1
