7570d8b7b51000373d817d63072f978bf5501c7a
Maksim Shuvaev <moksshuv@yandex.ru>
2025-03-19 13:44:21 +0300

    19.03.2025

--- barbarian-blaster/RayPickerCamera/ray_picer_camera.gd
+++ barbarian-blaster/RayPickerCamera/ray_picer_camera.gd
@@ -8,11 +8,13 @@ func _process(_delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100
 	ray_cast_3d.force_raycast_update()
 	
-	print(ray_cast_3d.is_colliding())
 	if ray_cast_3d.is_colliding():
 		var collider = ray_cast_3d.get_collider()
-		if collider is GridMap:
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+		if collider is GridMap and Input.is_action_pressed("click"):
 			var collision_point = ray_cast_3d.get_collision_point()
 			var cell = gridmap.local_to_map(collision_point)
 			if gridmap.get_cell_item(cell) == 0:
 				gridmap.set_cell_item(cell, 1)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
