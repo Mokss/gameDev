a715056a74914e1583934553af116d63b5987ef1
Maksim Shuvaev <moksshuv@yandex.ru>
2025-03-24 21:26:41 +0300

    24.03.2025[2]

--- barbarian-blaster/Enemy/enemy.gd
+++ barbarian-blaster/Enemy/enemy.gd
@@ -1,12 +1,22 @@
 class_name Enemy extends PathFollow3D
 
 @export var speed: float = 2.5
+@export var max_health: int = 50
 
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		if current_health < 1:
+			queue_free()
+		
 @onready var base: Base = get_tree().get_first_node_in_group('base')
+
+func _ready() -> void:
+	current_health = max_health
 
 func _process(delta: float) -> void:
 	progress += delta * speed
 	
 	if progress_ratio > 0.98:
 		base.take_damage()
-		set_process(false)
+		queue_free()
--- barbarian-blaster/Turret/projectile.gd
+++ barbarian-blaster/Turret/projectile.gd
@@ -7,5 +7,14 @@ var direction: Vector3 = Vector3.FORWARD
 func _physics_process(delta: float) -> void:
 	position += direction * delta * speed
 
+
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("enemy_area"):
+		var enemy = area.get_parent()
+		if enemy is Enemy:
+			enemy.current_health -= 25
+		queue_free()
