312451959d78676b83dd837ebc817af0f5a0957f
Mokss <moksshuv@yandex.ru>
2025-05-02 21:09:56 +0300

    2.05.2025

--- sci_fi_fps/Player/player.gd
+++ sci_fi_fps/Player/player.gd
@@ -1,9 +1,11 @@
 class_name Player extends CharacterBody3D
 
+const SPEED = 5.0
+
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.0
 @onready var camera_pivot: Node3D = $CameraPivot
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 var mouse_motion: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
@@ -14,11 +16,14 @@ func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0:
+			velocity += get_gravity() * delta
+		else:
+			velocity.y += get_gravity().y * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * -get_gravity().y)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
