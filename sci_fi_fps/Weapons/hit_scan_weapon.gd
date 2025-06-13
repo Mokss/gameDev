063c41e289f6cb35c7de87a898a9c37bcf053f7e
Mokss <moksshuv@yandex.ru>
2025-06-14 00:11:43 +0300

    14.06.2025

---
+++ sci_fi_fps/Weapons/hit_scan_weapon.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+# выстрелы в минуту
+@export var fire_rate: float = 14.0;
+@onready var cooldown_timer: Timer = $CooldownTimer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cooldown_timer.is_stopped():
+			cooldown_timer.start(1.0 / fire_rate);
+			print("weapon fired");
+		
