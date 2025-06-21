45be262a0e88608566c84e92d79692a37f9c4284
Mokss <moksshuv@yandex.ru>
2025-06-21 23:47:03 +0300

    21.06.2025

--- sci_fi_fps/Player/player.gd
+++ sci_fi_fps/Player/player.gd
@@ -6,10 +6,14 @@ const SPEED = 5.0
 @export var fall_multiplier: float = 2.0
 @export var max_hitpoints: int = 100
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var animation_player: AnimationPlayer = $DamageTexture/AnimationPlayer
 
 var mouse_motion: Vector2 = Vector2.ZERO
 var hitpoints: int = max_hitpoints:
 	set(value):
+		if value < hitpoints:
+			animation_player.stop(false)
+			animation_player.play('TakeDamage')
 		hitpoints = value
 		print("PLayer " + str(hitpoints))
 		if hitpoints <= 0:
