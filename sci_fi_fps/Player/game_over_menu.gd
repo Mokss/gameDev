e5d1e3d23e47f34c4b2501fba3c1e40db7935e4f
Mokss <moksshuv@yandex.ru>
2025-06-26 01:05:41 +0300

    26.06.2025

--- sci_fi_fps/Player/game_over_menu.gd
+++ sci_fi_fps/Player/game_over_menu.gd
@@ -9,6 +9,7 @@ func game_over() -> void:
 
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
+	@warning_ignore("return_value_discarded")
 	get_tree().reload_current_scene()
 
 
