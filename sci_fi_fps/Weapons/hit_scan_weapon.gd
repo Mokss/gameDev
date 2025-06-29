811d3a174fc8ad2fa60382887f67535e1ed253d0
Mokss <moksshuv@yandex.ru>
2025-06-29 23:44:19 +0300

    29.06.2025

--- sci_fi_fps/Weapons/hit_scan_weapon.gd
+++ sci_fi_fps/Weapons/hit_scan_weapon.gd
@@ -7,6 +7,7 @@ extends Node3D
 @export var weapon_damage: int = 15
 @export var muzzle_flash: GPUParticles3D
 @export var sparks: PackedScene
+@export var automatic: bool = true
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
@@ -19,10 +20,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("fire"):
-		if cooldown_timer.is_stopped():
-			shoot()
-
+	if automatic:
+		if Input.is_action_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
+	else:
+		if Input.is_action_just_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
 
 func _physics_process(delta: float) -> void:
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 6.0)
