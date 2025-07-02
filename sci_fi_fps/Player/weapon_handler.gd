cad5d2c45b9d406c6b6486116819c9fdba8edfac
Mokss <moksshuv@yandex.ru>
2025-07-02 23:04:50 +0300

    2.07.2025

--- sci_fi_fps/Player/weapon_handler.gd
+++ sci_fi_fps/Player/weapon_handler.gd
@@ -18,8 +18,30 @@ func equip(active_weapon: Weapon) -> void:
 			child.visible = false
 			child.set_process(false)
 
+
+func get_current_index() -> int:
+	for index in get_child_count():
+		if get_child(index).visible:
+			return index
+	return 0 
+
+
+func next_weapon() -> void:
+	var index: int = get_current_index()
+	index = wrapi(index + 1, 0, get_child_count())
+	equip(get_child(index))
+
+func last_weapon() -> void:
+	var index: int = get_current_index()
+	index = wrapi(index - 1, 0, get_child_count())
+	equip(get_child(index))
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('weapon_1'):
 		equip(weapon_1)
 	elif  event.is_action_pressed("weapon_2"):
 		equip(weapon_2)
+	elif event.is_action_pressed("next_weapon"):
+		next_weapon()
+	elif event.is_action_pressed('last_weapon'):
+		last_weapon()
