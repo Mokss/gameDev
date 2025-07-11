003536fcbf0ad3d79d5aa32cc97c0bc8759bba5f
Mokss <moksshuv@yandex.ru>
2025-07-12 01:31:33 +0300

    12.07.2025

--- sci_fi_fps/Player/player.gd
+++ sci_fi_fps/Player/player.gd
@@ -5,10 +5,16 @@ const SPEED = 5.0
 @export var jump_height: float = 1.0
 @export var fall_multiplier: float = 2.0
 @export var max_hitpoints: int = 100
+@export var aim_multiplier: float = 0.7
+
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var animation_player: AnimationPlayer = $DamageTexture/AnimationPlayer
 @onready var game_over_menu: GameOverMeny = $GameOverMenu
 @onready var ammo_handler: AmmoHandler = %AmmoHandler
+@onready var smooth_camera: Camera3D = %SmoothCamera
+@onready var weapon_camera: Camera3D = %WeaponCamera
+@onready var smooth_camera_fov: float = smooth_camera.fov
+@onready var weapon_camera_fov: float = weapon_camera.fov
 
 
 var mouse_motion: Vector2 = Vector2.ZERO
@@ -25,6 +31,15 @@ var hitpoints: int = max_hitpoints:
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("aim"):
+		smooth_camera.fov = lerp(smooth_camera.fov, smooth_camera_fov * aim_multiplier, delta * 20.0)
+		weapon_camera.fov = smooth_camera.fov
+	else:
+		smooth_camera.fov = smooth_camera_fov
+		weapon_camera.fov = weapon_camera_fov
 
 
 func _physics_process(delta: float) -> void:
@@ -47,6 +62,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("aim"):
+			velocity.x *= aim_multiplier
+			velocity.z *= aim_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -58,6 +76,8 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion &&  Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
+			if Input.is_action_pressed("aim"):
+				mouse_motion *= aim_multiplier
 
 
 func handle_camera_rotation() -> void:
