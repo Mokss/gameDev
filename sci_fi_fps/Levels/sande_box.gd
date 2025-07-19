3df1afba344b658d7918fe41ffb7ec82aea2973a
Maksim Shuvaev <moksshuv@yandex.ru>
2025-07-20 01:34:24 +0300

    20.07.2025

--- sci_fi_fps/Levels/sande_box.gd
+++ sci_fi_fps/Levels/sande_box.gd
@@ -1,8 +1,20 @@
 extends Node3D
 
+var can_reload: bool = true
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("reload_thene"):
+	print("input frame start")
+	# Это полный буул щит, для избежания багов в Windows
+	# Windows в Godot 4, InputMap после reload_current_scene() не полностью очищаются
+	# и just_pressed не срабатывает при удержании клавиши между перезагрузками.
+	if event is InputEventKey and not event.pressed and not event.echo:
+		if event.keycode in [KEY_R, KEY_ALT]:
+			print("Modifier or R released")
+			can_reload = true
+	
+	if can_reload && Input.is_action_just_pressed("reload_thene"):
+		print('reload')
+		await get_tree().process_frame
 		@warning_ignore("return_value_discarded")
 		get_tree().reload_current_scene()
 	
