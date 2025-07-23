e27882aa73939ec1e48fbdfba37726a053f86e07
Maksim Shuvaev <moksshuv@yandex.ru>
2025-07-24 00:54:16 +0300

    24.07.2025

--- sci_fi_fps/global_scripts/gobal_input.gd
+++ sci_fi_fps/global_scripts/gobal_input.gd
@@ -1,19 +1,16 @@
-extends Node3D
+extends Node
 
 var can_reload: bool = true
 
 func _input(event: InputEvent) -> void:
-	print("input frame start")
 	# Это полный буул щит, для избежания багов в Windows
 	# Windows в Godot 4, InputMap после reload_current_scene() не полностью очищаются
 	# и just_pressed не срабатывает при удержании клавиши между перезагрузками.
 	if event is InputEventKey and not event.pressed and not event.echo:
 		if event.keycode in [KEY_R, KEY_ALT]:
-			print("Modifier or R released")
 			can_reload = true
 	
 	if can_reload && Input.is_action_just_pressed("reload_thene"):
-		print('reload')
 		await get_tree().process_frame
 		@warning_ignore("return_value_discarded")
 		get_tree().reload_current_scene()
