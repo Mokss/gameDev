84a9ceb4807ab2ff85bdfc65969ab386beddacde
Maksim Shuvaev <moksshuv@yandex.ru>
2025-11-21 01:37:38 +0300

    21.11.2025

--- my_laboratory/physics/domino sandbox/domino/domino.gd
+++ my_laboratory/physics/domino sandbox/domino/domino.gd
@@ -1,9 +1,12 @@
 extends RigidBody3D
 
 @onready var variants: Node3D = $Variants
+@onready var _6_6: Node3D = $"Variants/6_6"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_6_6.visible = false;
+	
 	var dominos_count: int = variants.get_child_count()
 	
 	if dominos_count > 0:
